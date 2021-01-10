75ab3985f59847319a3a8d8e46a9bb35cf1cb159
olrol <olroldugin@yandex.com>
2021-01-10 16:50:48 +0400

    Добавил некоторые константы и глобальные переменные

---
+++ Constants.py
@@ -0,0 +1,16 @@
+# 'NS'(Not Stated) - те значения, которые еще пока не указаны
+WIDTH = 950
+HEIGHT = 750
+FPS = 60
+MAIN_TANK_1_SKIN_PATH = 'путь к скину главного танка'
+MAIN_TANK_2_SKIN_PATH = 'путь к скину второго главного танка'
+BOT_LITE_TANK_SKIN_PATH = 'путь к скину легкого танка'
+BOT_MEDIUM_TANK_SKIN_PATH = 'путь к скину среднего такнка'
+BOT_HEAVY_TANK_SKIN_PATH = 'путь к скину тяжелого танка'
+MAIN_MENU_MUSIC_PATH = 'путь к заглавной музыке игры'
+CLICK_BUTTON_SOUND_PATH = 'путь к звуку нажатия кнопки'
+DEFAULT_MAIN_TANK_SKIN_PATH = 'путь к стандартному скину танка'
+RED_MAIN_TANK_SKIN_PATH = 'путь к красному скину танка'
+GREEN_MAIN_TANK_SKIN_PATH = 'путь к зеленому скину танка'
+DARK_GREY_MAIN_TANK_SKIN_PATH = 'путь к темно-серому скину танка'
+
